7aa112dca1fdc9a36b65d169c37131e87dd17e93
imlnr <imlnr2002@gmail.com>
2024-05-16 14:11:00 +0530

    conditional rendering for cart

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -41,7 +41,7 @@ const Cart = () => {
       {loading && <Box sx={{ position: 'absolute', top: 'calc(50% - 70px)', left: "calc(50% - 50px)", zIndex: "1" }}><Loading /></Box>}
       <Box minHeight={"100vh"} position={'relative'} paddingTop={"20px"} sx={{ backgroundColor: "#f8f9fa", filter: loading ? "blur(5px)" : 'none' }}>
         <Grid container paddingX={isSmallScreen ? "1%" : "3%"} spacing={isSmallScreen ? 1 : 2}>
-          <Grid item xs={12} sm={8}>
+          <Grid item xs={12} sm={cart.length == 0 ? 12 : 8}>
             <Box sx={{ backgroundColor: 'white', borderRadius: '8px', padding: '25px' }}>
               <Typography variant='h4'>Shopping Cart</Typography>
               {
@@ -55,7 +55,7 @@ const Cart = () => {
               }
               {cart.length === 0 ? (
                 <Box width={"100%"} sx={{ display: 'flex', gap: "5px", alignItems: "center", justifyContent: "center", flexDirection: 'column' }}>
-                  <img style={{ margin: "0 auto" }} width={"50%"} src={empty_cart} alt="Empty Cart" />
+                  <img style={{ margin: "0 auto" }} width={"30%"} src={empty_cart} alt="Empty Cart" />
                   <Typography fontSize={'medium'}>Your cart is empty!</Typography>
                   <Typography fontSize={'small'}>Add items to it now</Typography>
                   <Button onClick={() => navigate('/products/page/1')} variant='contained'>Shop now</Button>
@@ -74,29 +74,31 @@ const Cart = () => {
               }
             </Box>
           </Grid>
-          <Grid item xs={12} sm={4}>
-            <Box sx={{ backgroundColor: 'white', borderRadius: '8px', padding: '20px' }}>
-              <Typography variant='h6'>Subtotal ({cart.length} items): <b>${total.toFixed(2)}</b></Typography>
-              <FormGroup>
-                <FormControlLabel control={<Checkbox />} label="This order contains a gift" />
-              </FormGroup>
-              <Button sx={{ marginBottom: "10px" }} onClick={() => navigate('/checkout', { state: { count: total } })} fullWidth variant='contained'>Proceed to Buy</Button>
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ArrowDropDownIcon />}
-                  aria-controls="panel2-content"
-                  id="panel2-header"
-                >
-                  <Typography>EMI Available</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                  <Typography fontSize={'small'}>
-                    Your order qualifies for EMI with valid credit cards (not available on purchase of Gold, Jewelry, Gift cards and Amazon pay balance top up). <Link to='#'>Learn more</Link>
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-            </Box>
-          </Grid>
+          {cart.length == 0 ? null :
+            <Grid item xs={12} sm={4}>
+              <Box sx={{ backgroundColor: 'white', borderRadius: '8px', padding: '20px' }}>
+                <Typography variant='h6'>Subtotal ({cart.length} items): <b>${total.toFixed(2)}</b></Typography>
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox />} label="This order contains a gift" />
+                </FormGroup>
+                <Button sx={{ marginBottom: "10px" }} onClick={() => navigate('/checkout', { state: { count: total } })} fullWidth variant='contained'>Proceed to Buy</Button>
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ArrowDropDownIcon />}
+                    aria-controls="panel2-content"
+                    id="panel2-header"
+                  >
+                    <Typography>EMI Available</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography fontSize={'small'}>
+                      Your order qualifies for EMI with valid credit cards (not available on purchase of Gold, Jewelry, Gift cards and Amazon pay balance top up). <Link to='#'>Learn more</Link>
+                    </Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            </Grid>
+          }
         </Grid>
       </Box>
     </>
