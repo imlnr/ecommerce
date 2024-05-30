085355e4742ce264a7829cb333f332b3cfb7444e
imlnr <imlnr2002@gmail.com>
2024-05-30 21:31:04 +0530

    changed the remove all button

---
+++ frontend/src/components/ResponsiveDialog.jsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { Delete } from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
+import { clear_cart } from '../redux/appReducer/action';
+
+export default function ResponsiveDialog() {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const dispatch = useDispatch();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        dispatch(clear_cart());
+        setOpen(false);
+    };
+
+    return (
+        <React.Fragment>
+            <Button color='error' startIcon={<Delete />} variant="contained" onClick={handleClickOpen}>
+                Clear Cart
+            </Button>
+            <Dialog
+                fullScreen={fullScreen}
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="responsive-dialog-title"
+            >
+                <DialogTitle id="responsive-dialog-title">
+                    {"Remove all Items?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are ytou sure you want to remove all the items on cart?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus onClick={() => setOpen(false)}>
+                        Disagree
+                    </Button>
+                    <Button onClick={handleClose} autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
+    );
+}
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { clear_cart, get_cart_items } from '../redux/appReducer/action';
+import { get_cart_items } from '../redux/appReducer/action';
 import { useDispatch, useSelector } from 'react-redux';
 import { Grid, Box, Typography, Button, Accordion, AccordionDetails, AccordionSummary, FormGroup, FormControlLabel, Checkbox, useMediaQuery } from '@mui/material';
 import CartLargeCard from '../components/CartLargeCard';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { Link, useNavigate } from 'react-router-dom';
 import Loading from '../components/Loading';
-import Delete from '@mui/icons-material/Delete';
 import empty_cart from '../assets/empty.jpg'
+import ResponsiveDialog from '../components/ResponsiveDialog';
 
 
 const Cart = () => {
@@ -41,11 +41,11 @@ const Cart = () => {
       {loading && <Box sx={{ position: 'absolute', top: 'calc(50% - 70px)', left: "calc(50% - 50px)", zIndex: "1" }}><Loading /></Box>}
       <Box minHeight={"100vh"} position={'relative'} paddingTop={"20px"} sx={{ backgroundColor: "#f8f9fa", filter: loading ? "blur(5px)" : 'none' }}>
         <Grid container paddingX={isSmallScreen ? "1%" : "3%"} spacing={isSmallScreen ? 1 : 2}>
-          <Grid item xs={12} sm={cart.length == 0 ? 12 : 8}>
+          <Grid item xs={12} sm={cart.length === 0 ? 12 : 8}>
             <Box sx={{ backgroundColor: 'white', borderRadius: '8px', padding: '25px' }}>
               <Typography variant='h4'>Shopping Cart</Typography>
               {
-                cart.length != 0 ?
+                cart.length !== 0 ?
                   <Box sx={{ display: "flex", justifyContent: "space-between" }}>
                     <Typography>
                       Deselect All Items
@@ -66,15 +66,16 @@ const Cart = () => {
                 ))
               )}
               {
-                cart.length != 0 ?
+                cart.length !== 0 ?
                   <Box sx={{ display: "flex", alignItems: 'center', justifyContent: "space-between", marginY: "10px" }}>
-                    <Button onClick={() => dispatch(clear_cart())} variant='contained' color='error' startIcon={<Delete />} disabled={cart.length == 0}>Clear Cart</Button>
+                    <ResponsiveDialog />
+                    {/* <Button onClick={() => dispatch(clear_cart())} variant='contained' color='error' startIcon={<Delete />} disabled={cart.length == 0}>Clear Cart</Button> */}
                     <Typography variant='h6'>Subtotal ({cart.length} items): <b>${total.toFixed(2)}</b></Typography>
                   </Box> : null
               }
             </Box>
           </Grid>
-          {cart.length == 0 ? null :
+          {cart.length === 0 ? null :
             <Grid item xs={12} sm={4}>
               <Box sx={{ backgroundColor: 'white', borderRadius: '8px', padding: '20px' }}>
                 <Typography variant='h6'>Subtotal ({cart.length} items): <b>${total.toFixed(2)}</b></Typography>
